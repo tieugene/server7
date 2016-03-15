80732c08dd9a8a17506e30eef40301c2848fe8e1
TI_Eugene <ti.eugene@gmail.com>
2016-03-15 12:14:19 +0300

    SMB QC OK

--- sh/mk_smbusers.sh
+++ sh/mk_smbusers.sh
@@ -3,6 +3,7 @@
 for i in `getent passwd | gawk -F'[/:]' '{print $1}' | grep ^user | sort`
 do
     smbldap-usermod -a $i
+    # Can't call method "get_value" on an undefined value at /usr/sbin/smbldap-usermod line 223.
     PASS=pass`echo $i | cut -b 5-6`
     echo $PASS | smbldap-passwd -s -p $i
     smbldap-groupmod -m $i Domain\ Users
